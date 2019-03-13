bba3f0d99225b72fbe7fc96dd6aa7aa55fc4c4a7
Ruchi <ruchikharwar4@gmail.com>
2019-03-13 16:57:10 +0530

    eventlist added

---
+++ client/src/components/user/Fight.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {connect} from "react-redux"
+import {getEvents} from '../../actions/event'
+
+
+class Fight extends Component {
+  constructor(){
+    super();
+    this.state = {
+      currentIndex: 0
+    }
+  }
+
+  handleNextFight = () => {
+    const maxIndex = this.props.data.length - 1;
+    console.log(this.state.currentIndex);
+
+    this.setState(state => {
+      const nextIndex = state.currentIndex >= maxIndex ? 0 : state.currentIndex + 1;
+      return {
+        currentIndex: nextIndex
+      }
+    })
+  }
+
+  handlePrevFight = () => {
+    const maxIndex = this.props.data.length - 1;
+    this.setState(state => {
+      return {
+        currentIndex: state.currentIndex <= 0 ? maxIndex : state.currentIndex - 1
+      }
+    })
+  }
+
+
+  render() {
+    const fight = this.props.data;
+    const event = this.props.event;
+    console.log(this.props.data)
+    return (
+      <div>
+        <div>
+          <img src={fight[this.state.currentIndex].player1.image} />
+          <img src={fight[this.state.currentIndex].player2.image} />
+          <div>{fight[this.state.currentIndex].title}</div>
+        </div>
+      <button className="nextfight-btn" onClick={this.handleNextFight}>Next</button>
+      <button className="prevfight-btn" onClick={this.handlePrevFight}>Prev</button>
+      </div>
+    )
+  }
+}
+
+
+
+export default connect()(Fight);
