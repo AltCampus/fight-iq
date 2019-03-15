9123baf5a88098fbb3f2dcdc4db9eae4778ffdf8
ashish4497 <ak8393267@gmail.com>
2019-03-15 15:13:25 +0530

    style the events

--- client/src/App.js
+++ client/src/App.js
@@ -12,32 +12,46 @@ import AdminEventDetail from "./components/admin/events/AdminEventDetail";
 import AddFight from "./components/admin/fights/AddFight";
 import AddPlayer from "./components/admin/AddPlayer";
 import AddResult from "./components/admin/fights/AddResult";
-import EventCards from './components/user/EventCards';
+import EventCards from "./components/user/EventCards";
 import DisplayPlayersList from "./components/admin/DisplayPlayersList";
-
 
 class App extends Component {
 	render() {
 		return (
 			<BrowserRouter>
 				<div className='App'>
-				<Header />
-				<Header2 />
+					<Header />
+					<Header2 />
 					<Switch>
 						<Route exact path='/' component={Events} />
 						<Route exact path='/admin' component={Admin} />
 						<Route exact path='/admin/event' component={AdminAddEvent} />
-						<Route exact path='/admin/event/:eventid' component={AdminEventDetail}/>
-						<Route exact path='/admin/:eventid/edit' component={AdminAddEvent}/>
+						<Route
+							exact
+							path='/admin/event/:eventid'
+							component={AdminEventDetail}
+						/>
+						<Route
+							exact
+							path='/admin/:eventid/edit'
+							component={AdminAddEvent}
+						/>
 						<Route exact path='/admin/:eventid/fights' component={AddFight} />
-						<Route exact path='/admin/:eventid/fights/:fightid/edit' component={AddFight}/>
-						<Route exact path='/admin/:eventid/fights/:fightid/result' component={AddResult}/> 
+						<Route
+							exact
+							path='/admin/:eventid/fights/:fightid/edit'
+							component={AddFight}
+						/>
+						<Route
+							exact
+							path='/admin/:eventid/fights/:fightid/result'
+							component={AddResult}
+						/>
 						<Route exact path='/admin/players/add' component={AddPlayer} />
 						<Route exact path='/admin/players' component={DisplayPlayersList} />
-            <Route path='/login' component={Login} />
-            <Route path='/register' component={Register} />
-            <Route path='/events' component={EventCards} />
-
+						<Route path='/login' component={Login} />
+						<Route path='/register' component={Register} />
+						<Route path='/events' component={EventCards} />
 					</Switch>
 				</div>
 			</BrowserRouter>
@@ -46,4 +60,3 @@ class App extends Component {
 }
 
 export default App;
- 
