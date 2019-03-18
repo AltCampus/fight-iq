c87ed9bd44646446ce9aecb569113f45d4e5d1f8
logicallycorrect <1994.ravi.com>
2019-03-18 09:27:38 +0530

    update prediction

--- client/src/components/user/predict/PredictChooseFighter.js
+++ client/src/components/user/predict/PredictChooseFighter.js
@@ -3,9 +3,19 @@ import { connect } from "react-redux";
 
 class PredictChooseFighter extends Component {
 	render() {
+		console.log(this.props, "props")
 		return (
 				<div className='PredictChooseFighter'>
-
+					<div className='img-section'>
+					<div className='player1'>
+						<img src={this.props.player1.image} />
+						<p>{this.props.player1.name}</p>
+					</div>
+					<div className='player2'>
+					<img src={this.props.player2.image} />
+						<p>{this.props.player2.name}</p>
+					</div>
+					</div>
 				</div>
 		);
 	}
