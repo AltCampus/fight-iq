fcda6989a6eb8efda91b3974e0e57837e0e79447
ashish4497 <ak8393267@gmail.com>
2019-03-18 14:57:05 +0530

    condetional reandering in events page done

--- client/src/components/user/EventDetails.js
+++ client/src/components/user/EventDetails.js
@@ -3,43 +3,40 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { getEvent } from "../../actions";
 import Fight from "./EventDetailsFight";
-import './style.scss';
+import "./style.scss";
 
 class EventDetails extends Component {
 	componentDidMount() {
-		this.props.dispatch(getEvent(this.props.match.params.eventid));		
+		this.props.dispatch(getEvent(this.props.match.params.eventid));
 	}
 
-
 	redirectUser = (success, errorMsg = "") => {
-		if (success){
-			this.props.history.push('/');
+		if (success) {
+			this.props.history.push("/");
 		} else {
 			this.setState({
 				error: errorMsg
-			})
+			});
 		}
-	}
+	};
 
 	render() {
-		let { event } = this.props;
-		const { fights } = this.props;
-		console.log(event, "raviravi")
+		let { event, fights, isLogged } = this.props;
 
 		return (
-			<div className="event-detail-page">
-				<div className="event-detail-banner">
-					<div className="event-title">{event.title}</div>
-					<div className="event-location">{event.location}</div>
-					<div className="event-date_time">{event.date_time}</div>
+			<div className='event-detail-page'>
+				<div className='event-detail-banner'>
+					<div className='event-title'>{event.title}</div>
+					<div className='event-location'>{event.location}</div>
+					<div className='event-date_time'>{event.date_time}</div>
 				</div>
 				<div className='AdminEventDetail'>
-					<div className="event-bottom-section">
-					</div>
-					<h5 className="fights">Fights: </h5>
-					{
-						fights && fights.map(fight=><Fight key={fight._id} fight={fight} eventid={event._id}/>)
-					}
+					<div className='event-bottom-section' />
+					<h5 className='fights'>Fights: </h5>
+					{fights &&
+						fights.map((fight) => (
+							<Fight key={fight._id} fight={fight} eventid={event._id} />
+						))}
 				</div>
 			</div>
 		);
@@ -49,7 +46,8 @@ class EventDetails extends Component {
 const mapStateToProps = (state) => {
 	return {
 		event: state.event,
-		fights: state.event.fight
+		fights: state.event.fight,
+		isLogged: state.isLogged
 	};
 };
 
