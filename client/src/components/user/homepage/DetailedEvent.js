13ce7ce0dde8119b0097695c7bdb112dea8fc669
ashish4497 <ak8393267@gmail.com>
2019-03-19 15:14:31 +0530

    events page condesional reandering is done

--- client/src/components/user/homepage/DetailedEvent.js
+++ client/src/components/user/homepage/DetailedEvent.js
@@ -1,31 +1,61 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 class DetailedEvent extends Component {
 	render() {
-		let {event} = this.props;
-		let mainFight = event.fight && event.fight.find(fight=>fight.type=='mainEvent');
+		let { event, isLogged } = this.props;
+		let mainFight =
+			event.fight && event.fight.find((fight) => fight.type == "mainEvent");
 
-		let eventDate = event.date_time && event.date_time.split('T')[0];
-		let eventTime = event.date_time && event.date_time.split('T')[1];
-		console.log(mainFight)
+		let eventDate = event.date_time && event.date_time.split("T")[0];
+		let eventTime = event.date_time && event.date_time.split("T")[1];
 		return (
-	          <Link to={`events/${event._id}`}>
-							<div className='DetailedEvent'>
-			    			<div className='event-title'>{event.title}</div>
-			    					<div className='event-images'>
-											<img className='player-1' src={mainFight.player1.image}/>
-											<img className='player-2' src={mainFight.player2.image}/>
-										</div>
-								<div className='location-info'>
-									<p className='txt-dec'>{eventTime}, {eventDate} </p>
-									<p className='title-location'>{event.location}</p>
-								</div>
-								<button className='Prediction-button'>Predict</button>
-	            </div>
-	          </Link>
-			);
-		}
+			<div>
+				{isLogged ? (
+					<Link to={`events/${event._id}`}>
+						<div className='DetailedEvent'>
+							<div className='event-title'>{event.title}</div>
+							<div className='event-images'>
+								<img className='player-1' src={mainFight.player1.image} />
+								<img className='player-2' src={mainFight.player2.image} />
+							</div>
+							<div className='location-info'>
+								<p className='txt-dec'>
+									{eventTime}, {eventDate}{" "}
+								</p>
+								<p className='title-location'>{event.location}</p>
+							</div>
+							<button className='Prediction-button'>Predict</button>
+						</div>
+					</Link>
+				) : (
+					<Link to={`/login`}>
+						<div className='DetailedEvent'>
+							<div className='event-title'>{event.title}</div>
+							<div className='event-images'>
+								<img className='player-1' src={mainFight.player1.image} />
+								<img className='player-2' src={mainFight.player2.image} />
+							</div>
+							<div className='location-info'>
+								<p className='txt-dec'>
+									{eventTime}, {eventDate}{" "}
+								</p>
+								<p className='title-location'>{event.location}</p>
+							</div>
+							<button className='Prediction-button'>Predict</button>
+						</div>
+					</Link>
+				)}
+			</div>
+		);
 	}
+}
 
-	export default DetailedEvent;
+const mapStateToProps = (state) => {
+	return {
+		isLogged: state.isLogged
+	};
+};
+
+export default connect(mapStateToProps)(DetailedEvent);
