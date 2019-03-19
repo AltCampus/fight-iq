06110e6eea9e97320929bca29fc9e37818c25c3b
Vishwa <vishwanathovi@gmail.com>
2019-03-19 21:56:21 +0530

    profile css improvement V0.1

--- client/src/components/user/profile/UserPrediction.js
+++ client/src/components/user/profile/UserPrediction.js
@@ -3,19 +3,29 @@ import React, { Component } from "react";
 class UserPrediction extends Component {
 	render() {
 		let { prediction } = this.props;
-		console.log(prediction, "data in user predication.js line6");
+		let resultPresent = !!prediction.fightid.result;
 		return (
 			<div className='UserPrediction'>
 				<div className='UserPrediction-event-details'>
 					<div>Event: {prediction.eventid.title}</div>
 					<div>Fight: {prediction.fightid.title}</div>
 				</div>
-				<div className='UserPrediction-result'>
-					<div className='UserPrediction-header'>Results: </div>
-					<div>Winner: {prediction.fightid.result.winner.name}</div>
-					<div>Type: {prediction.fightid.result.type}</div>
-					<div>Round: {prediction.fightid.result.round}</div>
-				</div>
+				{
+					resultPresent? 
+					(
+						<div className='UserPrediction-result'>
+							<div className='UserPrediction-header'>Results: </div>
+							<div>Winner: {prediction.fightid.result.winner.name}</div>
+							<div>Type: {prediction.fightid.result.type}</div>
+							<div>Round: {prediction.fightid.result.round}</div>
+						</div>
+					): (
+						<div className='UserPrediction-result'>
+							Event yet to happen!
+						</div>
+
+					)
+				}
 				<div className='UserPrediction-prediction'>
 					<div className='UserPrediction-header'>Prediction: </div>
 					<div>Winner: {prediction.winner.name}</div>
