3740b44c7a15558f25e110e2571bea5ae35a1f66
Vishwa <vishwanathovi@gmail.com>
2019-03-19 22:30:09 +0530

    profile improvement V0.2

--- client/src/components/user/profile/UserPrediction.js
+++ client/src/components/user/profile/UserPrediction.js
@@ -7,14 +7,13 @@ class UserPrediction extends Component {
 		return (
 			<div className='UserPrediction'>
 				<div className='UserPrediction-event-details'>
-					<div>Event: {prediction.eventid.title}</div>
-					<div>Fight: {prediction.fightid.title}</div>
+					<div className="event-name">{prediction.eventid.title}</div>
+					<div className="fight-name">{prediction.fightid.title}</div>
 				</div>
 				{
 					resultPresent? 
 					(
 						<div className='UserPrediction-result'>
-							<div className='UserPrediction-header'>Results: </div>
 							<div>Winner: {prediction.fightid.result.winner.name}</div>
 							<div>Type: {prediction.fightid.result.type}</div>
 							<div>Round: {prediction.fightid.result.round}</div>
@@ -27,7 +26,6 @@ class UserPrediction extends Component {
 					)
 				}
 				<div className='UserPrediction-prediction'>
-					<div className='UserPrediction-header'>Prediction: </div>
 					<div>Winner: {prediction.winner.name}</div>
 					<div>Type: {prediction.type}</div>
 					<div>Round: {prediction.round}</div>
