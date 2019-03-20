2ed574dc74fe995bfe06258333d9aa628104c578
Ruchi <ruchikharwar4@gmail.com>
2019-03-20 12:33:18 +0530

    added spinner

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         use: { loader: 'babel-loader' },
       },
       {
-        test: /\.scss$/,
+        test: /\.(css|scss)$/,
         use: [
           { loader: MiniCssExtractPlugin.loader },
           {
