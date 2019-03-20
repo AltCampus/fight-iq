39e845383c9a2771ee2d24c6a06838f6d577d548
Vishwa <vishwanathovi@gmail.com>
2019-03-20 09:47:54 +0530

    profile stat format fix

--- client/src/components/user/profile/UserStats.js
+++ client/src/components/user/profile/UserStats.js
@@ -11,7 +11,7 @@ class UserStats extends Component {
 					</div>
 					<div className="user-prediction">
 						<div className="user-stats-header">Win Accuracy</div>
-						<div className="user-stats">{user.accuracy} %</div>
+						<div className="user-stats">{user.accuracy || 0} %</div>
 					</div>
 					<div className="user-level">
 						<div className="user-stats-header">Level</div>
